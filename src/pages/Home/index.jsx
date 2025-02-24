ad1cc77d063bf6510fe3e0c8591db5483a64a0ff
sofiasaless <sofiasaleswk@gmail.com>
2025-02-24 19:48:26 -0300

    component: titulo para seções

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,12 +2,15 @@ import './style.css'
 
 import Header from '../../components/Header'
 import CardProduto from '../../components/CardProduto';
+import Titulo from '../../components/Titulo/Titulo';
 
 export default function Home () {
   return (
     <div style={{backgroundColor: '#e8e8e8'}}>
       <Header/>
       <main className='container'>
+
+        <Titulo titulo={"Enfeites de parede"} upper={true}/>
 
         <section className='container py-5 gap-4 justify-content-center'>
 
