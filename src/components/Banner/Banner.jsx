048dce6e1e42f673b379898958ae55382e56f18c
sofiasaless <sofiasaleswk@gmail.com>
2025-02-28 20:52:13 -0300

    feat: seção de materiais educativos

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,17 +1,14 @@
 import './style.css'
 
-// assets e outros
-import img1 from '../../assets/banner/creative-thinking-animate.svg'
-
-export default function Banner( { conteudoTXT } ) {
+export default function Banner( { imgImport, conteudoTXT } ) {
   return (
     <>
-      <div className="container-banner py-4 d-flex flex-column">
+      <div className="container-banner py-4 my-4 d-flex flex-column">
         
         <div className='container container-banner-interno d-flex align-items-center justify-content-center gap-5'>
           
           <div className='banner-imgs'>
-            <img src={img1} className='img-banner' />
+            <img src={imgImport} className='img-banner' />
           </div>
           
           <div className='barra-banner rounded-4'></div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,15 @@
 import './style.css'
 
+// componentes
 import Header from '../../components/Header/Header'
 import CardProduto from '../../components/CardProduto/CardProduto'
 import Titulo from '../../components/Titulo/Titulo';
 import MobileHeader from '../../components/MobileHeader/MobileHeader';
 import Banner from '../../components/Banner/Banner';
+
+// assets
+import img1 from '../../assets/banner/creative-thinking-animate.svg'
+import img2 from '../../assets/banner/kindergarten-student-animate.svg'
 
 export default function Home() {
   return (
@@ -33,8 +38,36 @@ export default function Home() {
       </main>
 
       <main>
-        <Banner 
+        <Banner
+          imgImport={img2} 
           conteudoTXT={"Personalizamos cada peça com carinho, utilizando EVA, papel crepom, TNT e muito mais para criar um ambiente lúdico e inspirador para suas crianças."}
+        />
+      </main>
+
+      <main className='container'>
+
+        <Titulo titulo={"Materiais educativos"} upper={true} />
+
+        <section className='container py-5 gap-4 justify-content-center'>
+
+          <CardProduto />
+          <CardProduto />
+          <CardProduto />
+          <CardProduto />
+
+        </section>
+
+        <section className='container d-flex justify-content-center'>
+          <div className='bt-verTodos p-3 rounded-4 mt-1 mb-4'>Ver todos os produtos</div>
+        </section>
+        
+
+      </main>
+      
+      <main>
+        <Banner
+          imgImport={img1}
+          conteudoTXT={"Você imagina, nós criamos! Também oferecemos decorações personalizadas feitas do jeitinho que você quiser. Nós criamos cada detalhe com carinho para deixar o ambiente ainda mais acolhedor e divertido."}
         />
       </main>
 
